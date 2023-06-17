d22ccc1bfee45360f857d964a9ad1933c5a0cdf2
Tony Qu <tonyqus@gmail.com>
2023-06-17 14:22:39 +0800

    fix bug

--- IPFox.Tests/IPGeodataRepositoryTests.cs
+++ IPFox.Tests/IPGeodataRepositoryTests.cs
@@ -5,20 +5,20 @@ namespace IPFox.Tests
         [Test]
         public async Task TestGetIPLocation_USAddr()
         {
-            var segment = await IPGeodataRepository.GetIPLocationAsync("8.8.8.8");
-            Assert.IsNotNull(segment);
-            Assert.That(segment.Detail.Country, Is.EqualTo("美国"));
-            Assert.That(segment.Detail.DataProvider, Is.EqualTo("Level3"));
+            var detail = await IPGeodataRepository.GetIPLocationAsync("8.8.8.8");
+            Assert.IsNotNull(detail);
+            Assert.That(detail.Country, Is.EqualTo("美国"));
+            Assert.That(detail.DataProvider, Is.EqualTo("Level3"));
         }
         [Test]
         public async Task TestGetIPLocation_DEAddr()
         {
-            var segment = await IPGeodataRepository.GetIPLocationAsync("104.76.204.20");
-            Assert.IsNotNull(segment);
-            Assert.That(segment.Detail.Country, Is.EqualTo("德国"));
-            Assert.That(segment.Detail.State, Is.EqualTo("法兰克福"));
-            Assert.That(segment.Detail.City, Is.EqualTo("法兰克福"));
-            Assert.That(segment.Detail.DataProvider, Is.EqualTo("阿卡迈"));
+            var detail = await IPGeodataRepository.GetIPLocationAsync("104.76.204.20");
+            Assert.IsNotNull(detail);
+            Assert.That(detail.Country, Is.EqualTo("德国"));
+            Assert.That(detail.State, Is.EqualTo("法兰克福"));
+            Assert.That(detail.City, Is.EqualTo("法兰克福"));
+            Assert.That(detail.DataProvider, Is.EqualTo("阿卡迈"));
         }
     }
 }
